a54c9542a0e3ea76cd67225acbc0bd1c78fa3d7e
HussainiMD <hussaini.crazystuff@gmail.com>
2025-12-01 17:07:18 +0530

    adding more comments for clarity

--- tests/file-handling/readCSVFile-library.ts
+++ tests/file-handling/readCSVFile-library.ts
@@ -1,3 +1,7 @@
+/*THIS IS NOT A PLAYWRIGHT TEST FILE. It is a typscript file */
+/*Transpile TS to JS code: npx tsc readCSVFile-library.ts */
+/*Run JS code on NODE: node readCSVFile-library.js*/
+
 /*we need to install this library 'csv-parse' in our project first */
 /*we also use fs and path modules as well */
 import { parse } from 'csv-parse/sync';
