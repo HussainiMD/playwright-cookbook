14e7385a44b5b0ef230bde7a2c7d0f354d86d23f
HussainiMD <hussaini.crazystuff@gmail.com>
2025-12-04 22:35:54 +0530

    mock an API used by UI

---
+++ tests/network/api/mocking/mockAPIResponse.spec.ts
@@ -0,0 +1,31 @@
+import {test, expect, Locator} from "@playwright/test";
+
+const listItems:Array<String> = ["Mandarin","Tangerine","Clementines"];
+
+function getMockJSON(itemsList: Array<String>): Array<Object> {
+  return itemsList.map((item,id) => {
+             return {name: item, id}
+         })  
+}
+
+test("API Mocking test", async ({ page }) => {
+   /*Note that this is NOT wild card path match, instead a very specific match*/ 
+  page.route(
+    "https://demo.playwright.dev/api-mocking/api/v1/fruits",  // this is API end point which is expected to return a JSON
+    async (route) => {
+      const json:Array<Object> = getMockJSON(listItems);
+      await route.fulfill({ json }); //send our mock data in API response
+    }
+  );
+
+  /*this web page internally hits above Mocked API endpoint */
+  await page.goto("https://demo.playwright.dev/api-mocking/"); 
+  await page.waitForLoadState('networkidle');
+
+  /*fetching all items from UI and verifying that they are from our mock list (of items) */
+  const uiListContainer:Locator = page.locator('xpath=//*[@id="root"]/div/div/div/ul');
+  const uiListItems:Array<String> = (await uiListContainer.first().innerText()).split('\n');
+  const isContainsAll:boolean = uiListItems.every(item => listItems.includes(item));
+  expect(isContainsAll).toBe(true);
+
+});
